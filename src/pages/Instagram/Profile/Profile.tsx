b97efcc019bc66b47fa5e7ed55bbc6262bee36e1
kimHyeonJu <kimhyesom1202@gmail.com>
2024-11-17 15:08:48 +0900

    Component: Create ProfileBody component

--- src/pages/Instagram/Profile/Profile.tsx
+++ src/pages/Instagram/Profile/Profile.tsx
@@ -1,10 +1,13 @@
+import styles from "@/pages/Instagram/Profile/Profile.module.css";
 import ProfileHeader from "@/components/Instagram/ProfileHeader/ProfileHeader";
+import ProfileBody from "@/components/Instagram/ProfileBody/ProfileBody";
 
 function Profile() {
   return (
-    <>
+    <section className={styles["profile-container"]}>
       <ProfileHeader />
-    </>
+      <ProfileBody />
+    </section>
   )
 }
 
