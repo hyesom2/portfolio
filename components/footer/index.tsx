52c5b729f930d9b694ffd2848357c55d5d124236
kimHyeonJu <kimhyesom1202@gmail.com>
2026-02-24 23:04:52 +0900

    [Refactor] content 내용 변경

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className='w-full border-t border-[#eee] py-[30px] px-5'>
       <div className='flex flex-col gap-2 mx-auto md:max-w-[1200px]'>
         <h2>김현주 | 프론트엔드 개발자</h2>
-        <p>&copy; 2025 KIMHYEONJU All rights reserved.</p>
+        <p>&copy; 2026 KIMHYEONJU All rights reserved.</p>
       </div>
     </footer>
   );
